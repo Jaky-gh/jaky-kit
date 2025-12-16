8307879de8c0921a4693f9b84161de8f24484652
Anthony Truong <anthonyytruong@gmail.com>
2025-12-15 22:50:22 -0500

    Add attributes to x-button

--- components/x-button/x-button.js
+++ components/x-button/x-button.js
@@ -4,6 +4,10 @@ fetch(new URL('./x-button.css', import.meta.url))
     .then(css => sheet.replaceSync(css));
 
 class XButton extends HTMLElement {
+    static get observedAttributes() {
+        return ['disabled', 'type'];
+    }
+
     constructor() {
         super();
         const root = this.attachShadow({ mode: 'open' });
@@ -13,13 +17,60 @@ class XButton extends HTMLElement {
                 <slot></slot>
             </button>
         `;
+
+        this._btn = root.querySelector('button');
+
+        this._onClick = (e) => {
+            // If disabled, block any action
+            if (this.disabled) {
+                e.preventDefault();
+                e.stopPropagation();
+                return;
+            }
+
+            this.dispatchEvent(new Event('click', { bubbles: true, composed: true }));
+        };
+
+        this._btn.addEventListener('click', this._onClick);
     }
 
     connectedCallback() {
-        this.shadowRoot.querySelector('button')
-            .addEventListener('click', () => {
-                console.log('<x-button> clicked!');
-            });
+        this._syncFromAttributes();
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (oldValue === newValue) return;
+        if (!this._btn) return;
+
+        if (name === 'disabled') {
+            this._btn.disabled = newValue !== null;
+        }
+
+        if (name === 'type') {
+            this._btn.type = newValue || 'button';
+        }
+    }
+
+    get disabled() {
+        return this.hasAttribute('disabled');
+    }
+
+    set disabled(v) {
+        if (v) this.setAttribute('disabled', '');
+        else this.removeAttribute('disabled');
+    }
+
+    get type() {
+        return this.getAttribute('type') ?? 'button';
+    }
+
+    set type(v) {
+        this.setAttribute('type', v ?? 'button');
+    }
+
+    _syncFromAttributes() {
+        this._btn.disabled = this.disabled;
+        this._btn.type = this.type;
     }
 }
 
